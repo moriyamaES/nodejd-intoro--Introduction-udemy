3bdf57b384120dedba2fe042b50ed5f12d3936be
kazuhiro <kazuhiro@localhost.localdomain>
2023-10-05 00:32:43 +0900

    Various updates

--- projects/sample/index.js
+++ projects/sample/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var parent = function () {
+  console.log("parent() is called.");
+  child();
+  console.log("parent() is executed.");
+};
+
+var child = function () {
+  console.log("child() is called.");
+  console.log("child() is executed.");
+};
+
+parent();
