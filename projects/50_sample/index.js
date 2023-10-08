58aee479a94cb15dcb7d518fff53a9a518347de8
kazuhiro <kazuhiro@localhost.localdomain>
2023-10-08 14:26:41 +0900

    Update

---
+++ projects/50_sample/index.js
@@ -0,0 +1,4 @@
+var path = require("path");
+
+console.log(path.normalize("./nodejs-intoro-Introduction-udemy/projects/40_sample/../50_sample/index.js"));
+console.log(path.normalize("/home/kazuhiro/nodejs-intoro-Introduction-udemy/projects/40_sample/../50_sample/index.js"));
